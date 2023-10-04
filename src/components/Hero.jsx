9426a1e40308788cae2027c6609eff7b352746f9
Jxk0be <jakeshoffner27@gmail.com>
2023-10-04 17:29:08 -0400

    style changes again...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,8 +14,8 @@ const fadeInAnimationVariants = {
 
 const Hero = () => {
   return (
-    <div id="/" className='w-full h-[80vh] md:h-[70vh] flex justify-center items-center'>
-      <motion.div variants={fadeInAnimationVariants} initial="initial" whileInView="animate" transition={{duration: 0.95}} viewport={{once: true}} className='grid grid-cols-1 md:grid-cols-2 gap-y-10 max-w-[1240px] mx-auto'>
+    <div id="/" className='w-full h-[100vh] md:h-[70vh] flex justify-center items-center'>
+      <motion.div variants={fadeInAnimationVariants} initial="initial" whileInView="animate" transition={{duration: 0.95}} viewport={{once: true}} className='md:grid hidden grid-cols-2 max-w-[1240px] mx-auto'>
         <div className='text-[70px] font-extrabold text-[#eaeff5] flex justify-center md:items-start flex-col md:text-start w-full text-center drop-shadow-lg px-5'>
           <h1>Ved.<span className='text-red-500'>jx</span></h1>
           <h1 className='text-[28px] md:text-[30px] font-bold'>Afforadable Web Solutions</h1>
@@ -26,6 +26,19 @@ const Hero = () => {
           <img src={HeroImage} />
         </div>
       </motion.div>
+
+      <div className='grid grid-cols-1 md:hidden gap-y-10 mx-auto'>
+        <div className='text-[70px] font-extrabold text-[#eaeff5] flex justify-center md:items-start flex-col md:text-start w-full text-center drop-shadow-lg px-5'>
+          <h1>Ved.<span className='text-red-500'>jx</span></h1>
+          <h1 className='text-[28px] md:text-[30px] font-bold'>Afforadable Web Solutions</h1>
+          <h1 className='text-[20px] md:text-[20px] font-semibold'>Based in <span className=''>Knoxville, TN</span></h1>
+        </div>
+
+        <div className='w-full'>
+          <img src={HeroImage} />
+        </div>
+      </div>
+
     </div>
   )
 }
