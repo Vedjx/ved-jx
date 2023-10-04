8994ac30893ebb509ed4285ae25c86c84de3b740
Jxk0be <jakeshoffner27@gmail.com>
2023-10-04 19:43:33 -0400

    embarassing mistake

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className='grid grid-cols-1 md:hidden gap-y-10 mx-auto'>
         <div className='text-[70px] font-extrabold text-[#eaeff5] flex justify-center md:items-start flex-col md:text-start w-full text-center drop-shadow-lg px-5'>
           <h1>Ved.<span className='text-red-500'>jx</span></h1>
-          <h1 className='text-[28px] md:text-[30px] font-bold'>Afforadable Web Solutions</h1>
+          <h1 className='text-[28px] md:text-[30px] font-bold'>Affordable Web Solutions</h1>
           <h1 className='text-[20px] md:text-[20px] font-semibold'>Based in <span className=''>Knoxville, TN</span></h1>
         </div>
 
